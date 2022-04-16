28c759a2cd5bc03133ba35dc96008fe3404c9304
Daniel-Jamison <danjam215@gmail.com>
2022-04-16 17:32:32 -0500

    Update Hallway.js

--- Hallway.js
+++ Hallway.js
@@ -3,7 +3,7 @@ window.onload = function init()
 {	
 	document.onkeydown = checkKey;
 	
-	var n = 32;
+	var n = 64;
 	var timestep = 50
 	
 	var steps = 0;
@@ -17,20 +17,22 @@ function Rect(Time,n)
 {	
 	var delta = 302/640
 
-	var scale = 5;
+	var scale = 1;
 	var canvas = document.getElementById("myCanvas");
 	width = Math.floor(window.innerWidth / scale);
 	height = Math.floor(window.innerHeight / scale);
 	canvas.width = width * scale;
 	canvas.height = height * scale;
 	
-	Hall(delta)						//Make lines going toward center
 	
-	Squares(Time,n,delta)			//Make Hall look like its moving
+	
+	Squares2(Time,n,delta)			//Make Hall look like its moving
 		
 	pos = 350*Math.sin(Time/30);	
 	
 	Shape(pos)						//Make object move around
+	
+	Hall(delta)						//Make lines going toward center
 		
 }
 
@@ -117,29 +119,109 @@ function Squares(Time,n,delta)
 function Squares2(Time,n,delta)
 {
 	
-	var t = Time % 100
+	var t = Time % 10
 	
-		var b = t/100
-				
-		var c = b*b*b
+	for (let s = 0; s < n; s++) 
+	{
+		var b1 = (2*s)/n + ((.2/n)*t)
+		var b2 = ((2*s)-1)/n + ((.2/n)*t)
+		var b3 = ((2*s)+1)/n + ((.2/n)*t)
+		//var b1 = b+(.05*(2*s))
+		//var b2 = b+(.05*(2*s+1))
 		
-		var d = (c*630)+10
+		pow = 10
+					
+		var e1 = Math.pow(b1,pow)
+		var e2 = Math.pow(b2,pow)
+		var e3 = Math.pow(b3,pow)
+		
+		var d1 = e1*640
+		var d2 = e2*640
+		var d3 = e3*640
 		
 		var c = document.getElementById("myCanvas");
 		var ctx = c.getContext("2d");
 		//var ctx = canvas.getContext('2d');
 		
-		ctx.fillStyle = "grey";
+		//ctx.fillStyle = "grey";
+		//ctx.fillStyle = `rgb(${Math.floor(255*((s/n))+t)},${Math.floor(255*((s/n))+t)},${Math.floor(255*((s/n))+t)})`;
+		ctx.fillStyle = `rgb(255,255,255)`;
 		
 		ctx.beginPath();
-		ctx.moveTo(640-d, 302-(d*delta));
-		ctx.lineTo(2*d, 302-(d*delta));
-		ctx.lineTo(640-(d+100), 302-((d+100)*delta));
-		ctx.lineTo(2*(d+100), 2*((d+100)*delta));
+		ctx.moveTo(640-d1, 302-(d1*delta));
+		ctx.lineTo(640+d1, 302-(d1*delta));
+		ctx.lineTo(640+d2, 302-(d2*delta));
+		ctx.lineTo(640-d2, 302-(d2*delta));
 		ctx.closePath();
 		ctx.stroke()
 		ctx.fill();
-	
+		
+		ctx.beginPath();
+		ctx.moveTo(640-d1, 302+(d1*delta));
+		ctx.lineTo(640+d1, 302+(d1*delta));
+		ctx.lineTo(640+d2, 302+(d2*delta));
+		ctx.lineTo(640-d2, 302+(d2*delta));
+		ctx.closePath();
+		ctx.stroke()
+		ctx.fill();
+		
+		ctx.beginPath();
+		ctx.moveTo(640-d1, 302+(d1*delta));
+		ctx.lineTo(640-d1, 302-(d1*delta));
+		ctx.lineTo(640-d3, 302-(d3*delta));
+		ctx.lineTo(640-d3, 302+(d3*delta));
+		ctx.closePath();
+		ctx.stroke()
+		ctx.fill();
+		
+		ctx.beginPath();
+		ctx.moveTo(640+d1, 302-(d1*delta));
+		ctx.lineTo(640+d1, 302+(d1*delta));
+		ctx.lineTo(640+d3, 302+(d3*delta));
+		ctx.lineTo(640+d3, 302-(d3*delta));
+		ctx.closePath();
+		ctx.stroke()
+		ctx.fill();
+		
+		ctx.fillStyle = `rgb(0,0,0)`;
+		
+		ctx.beginPath();
+		ctx.moveTo(640-d1, 302-(d1*delta));
+		ctx.lineTo(640+d1, 302-(d1*delta));
+		ctx.lineTo(640+d3, 302-(d3*delta));
+		ctx.lineTo(640-d3, 302-(d3*delta));
+		ctx.closePath();
+		ctx.stroke()
+		ctx.fill();
+		
+		ctx.beginPath();
+		ctx.moveTo(640-d1, 302+(d1*delta));
+		ctx.lineTo(640+d1, 302+(d1*delta));
+		ctx.lineTo(640+d3, 302+(d3*delta));
+		ctx.lineTo(640-d3, 302+(d3*delta));
+		ctx.closePath();
+		ctx.stroke()
+		ctx.fill();
+		
+		ctx.beginPath();
+		ctx.moveTo(640-d1, 302+(d1*delta));
+		ctx.lineTo(640-d1, 302-(d1*delta));
+		ctx.lineTo(640-d2, 302-(d2*delta));
+		ctx.lineTo(640-d2, 302+(d2*delta));
+		ctx.closePath();
+		ctx.stroke()
+		ctx.fill();
+		
+		ctx.beginPath();
+		ctx.moveTo(640+d1, 302-(d1*delta));
+		ctx.lineTo(640+d1, 302+(d1*delta));
+		ctx.lineTo(640+d2, 302+(d2*delta));
+		ctx.lineTo(640+d2, 302-(d2*delta));
+		ctx.closePath();
+		ctx.stroke()
+		ctx.fill();
+
+	}
 }
 
 function Shape(pos)
@@ -152,7 +234,7 @@ function Shape(pos)
 	var p = ((258*((pos-60)-((70*(pos-60))/218)))/(pos-60))-148
 	
 	var ctx = canvas.getContext('2d');
-	ctx.fillStyle = "grey";
+	ctx.fillStyle = "gray";
 	ctx.strokeStyle = "black";
 	ctx.lineWidth = -3;
 	
